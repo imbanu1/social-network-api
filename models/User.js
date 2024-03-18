66a8262f1e0c67116cdf48086b975d0cd600857f
ike imbanu <imbanu1@gmail.com>
2024-03-18 20:40:15 -0300

    added content to my user.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const { Thought } = require(".");
+
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true, 
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/], 
+      },
+      thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought', 
+      },
+      friends: {
+        type: Schema.Types.ObjectId,
+        ref: 'User', 
+      },
+    }
+)
+
