f46934d0176b51d4a3b55856989743ec4d260538
ike imbanu <imbanu1@gmail.com>
2024-03-18 20:46:24 -0300

    modified user,js

--- models/User.js
+++ models/User.js
@@ -15,14 +15,30 @@ const UserSchema = new Schema(
         unique: true,
         match: [/.+@.+\..+/], 
       },
-      thoughts: {
+      thoughts: [
+        {
         type: Schema.Types.ObjectId,
         ref: 'Thought', 
       },
-      friends: {
+    ],
+      friends: [
+      {
         type: Schema.Types.ObjectId,
         ref: 'User', 
       },
-    }
-)
+    ],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+    );
+    UserSchema.virtual("friendCount").get(function() {
+        return this.friends.length;
+    });
 
+    const User = model("User", UserSchema);
+
+module.exports = User;
