5013a8fe045fb7f594a8d92ef7cc24e5ac31638f
ike imbanu <imbanu1@gmail.com>
2024-03-18 21:06:56 -0300

    modified thought.js

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+const { Thought } = require('.');
+
+
+const thoughtSchema = new Schema(
+    {
+       thoughtText: {
+        type: String,
+        required: 'Please drop a thought',
+        minlength: 1,
+        maxlength: 280,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+reactions: [reactionSchema]
+},
+       {
+        toJSON: {
+
+            getters: true
+        },
+        id: false
+       }
+);
+
+    thoughtSchema.virtual('reactionCount').get(function() {
+        return this.reaction.length;
+    });
+
+    const Thought = model('Thought', thoughtSchema);
+
+
+    module.exports =Thought;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 const { Thought } = require(".");
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
     {
       username: {
         type: String,
@@ -35,10 +35,10 @@ const UserSchema = new Schema(
         id: false,
       }
     );
-    UserSchema.virtual("friendCount").get(function() {
+    userSchema.virtual("friendCount").get(function() {
         return this.friends.length;
     });
 
-    const User = model("User", UserSchema);
+    const User = model("User", userSchema);
 
 module.exports = User;
