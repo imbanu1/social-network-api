9ce98163ea07209d7f260b15f7fc68fc1da0ede0
ike imbanu <imbanu1@gmail.com>
2024-03-20 18:17:19 -0300

    changes to server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(routes);
 
-mongoose.connect('mongodb://localhost:27017/your_database_name', {
+mongoose.connect('mongodb://localhost:27017/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
