6fe76306b0d1efb3499a340a0722ac4ef849a1b7
ike imbanu <imbanu1@gmail.com>
2024-03-20 18:01:26 -0300

    content added to user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,3 +7,14 @@ const userController = {
             path: 'friends',
             select: '-__v'
         })
+        .select('-__v')
+        .sort({ _id: -1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    };
+
+    module.exports = userController;
